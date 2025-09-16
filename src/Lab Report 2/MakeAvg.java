6b37078946eb525c83f86b41bd5dda28942a5ac7
raahimanowar <raahimfardin@gmail.com>
2025-09-16 12:15:41 +0600

    feat: solve MakeAvg

---
+++ src/Lab Report 2/MakeAvg.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class MakeAvg {
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+        int A;
+        int B;
+        int C;
+        A = sc.nextInt();
+        C = sc.nextInt();
+
+        if ((A + C) % 2 == 0) {
+            B = (A + C) / 2;
+
+            System.out.println("B exists. B is: " + B);
+        } else  {
+            System.out.println("B doesn't exists");
+        }
+    }
+}
