d585bbc258d1f1b37a80b8e27b7b25bcb275e1be
raahimanowar <raahimfardin@gmail.com>
2025-09-17 21:19:30 +0600

    fix typo

--- src/labreport2/FindStrictAverage.java
+++ src/labreport2/FindStrictAverage.java
@@ -1,6 +1,8 @@
+package labreport2;
+
 import java.util.*;
 
-public class FindStrictAvergae {
+public class FindStrictAverage {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
